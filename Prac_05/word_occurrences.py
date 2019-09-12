e8cedc6a9233ebf02146cc542683db7402103007
AdamEdgar <adamedgar2101@gmail.com>
2019-09-12 10:49:31 +1000

    Upload Prac_05 Python Files

---
+++ Prac_05/word_occurrences.py
@@ -0,0 +1,10 @@
+split_sentence = {}
+sentence = input("Text: ")
+words = sentence.split()
+for word in words:
+    occurrence = split_sentence.get(word, 0)
+    split_sentence[word] = occurrence + 1
+words = list(split_sentence.keys())
+max_length = max((len(word) for word in words))
+for word in words:
+    print("{:{}}: {}".format(word, max_length, split_sentence[word]))
